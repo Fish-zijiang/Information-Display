c57fa56a6169235fb3fb97bb70b64bfd06010ccc
Your Name <you@example.com>
2026-08-24 10:13:52 +0800

    first upload

---
+++ src/client/kotlin/infodis/client/InformationDisplayClient.kt
@@ -0,0 +1,9 @@
+package infodis.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object InformationDisplayClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
